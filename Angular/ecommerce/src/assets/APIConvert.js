514bab12f2a03666a4e5723575cf4c9c8bd9bab0
Guilherme-LTS <glts.snf21@uea.edu.br>
2023-09-12 22:09:55 -0400

    API de conversao dolar, parcialmente resolvida.

--- Angular/ecommerce/src/assets/APIConvert.js
+++ Angular/ecommerce/src/assets/APIConvert.js
@@ -1,4 +1,3 @@
-// Primeiro, vamos criar uma função para obter a taxa de conversão atual de USD para BRL
 async function getConversionRate() {
   const response = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
   const data = await response.json();
@@ -7,17 +6,17 @@ async function getConversionRate() {
   return data.USDBRL.ask;
 }
 
-// Em seguida, vamos criar uma função para converter o preço em reais para dólares
-async function convertPrice() {
+function convertPrice() {
   const priceInReaisElement = document.getElementById('price-reais');
   const priceInDollarsElement = document.getElementById('price-dollars');
 
   const priceInReais = parseFloat(priceInReaisElement.textContent);
-  const conversionRate = await getConversionRate();
 
-  const priceInDollars = priceInReais / conversionRate;
-  priceInDollarsElement.textContent = priceInDollars.toFixed(2);
+  getConversionRate().then((conversionRate) => {
+    const priceInDollars = priceInReais / conversionRate;
+    priceInDollarsElement.textContent = priceInDollars.toFixed(2);
+  });
 }
 
-// Finalmente, vamos chamar a função convertPrice quando a página for carregada
-window.onload = convertPrice;
+// Chame a função convertPrice após 3 segundos (3000 milissegundos) que a página for carregada
+setTimeout(convertPrice, 1000);
